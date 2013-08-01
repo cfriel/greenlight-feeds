e6081340cbc933e512ba39535cf25f46c961ee63
Chris Friel <cfriel@gmail.com>
2013-08-01 01:42:52 -0400

    expanding feeds structure

--- client/feeds.js
+++ client/feeds.js
@@ -1,1 +1,14 @@
+ Meteor.Router.add({
+    '/feeds': function(path)
+    {
+	return 'feeds_page';
+    }
+});
+
+
+Template.feeds_page.rendered = function(){
+
+    $('#left-navbar').affix();
+
  
+}
--- package.js
+++ package.js
@@ -6,6 +6,9 @@ Package.on_use(function (api, where) {
     api.use('router', ['client', 'server']);
     api.use(['templating'], 'client');
     
+    api.add_files(['client/create_feed.html', 'client/create_feed.js', 'client/create_feed.css'], 'client');
+    api.add_files(['client/existing_feeds.html', 'client/existing_feeds.js', 'client/existing_feeds.css'], 'client');
+
     api.add_files('client/feeds.js', 'client');
     api.add_files('server/feeds.js', 'server');
 });

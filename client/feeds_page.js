81336e8fe5a64224723d01284c45dd702d7a16ea
Chris Friel <cfriel@gmail.com>
2013-08-10 22:34:24 -0400

    restricting datasets

--- client/feeds_page.js
+++ client/feeds_page.js
@@ -4,7 +4,7 @@ Template.feeds_page.created = function()
     var description = "The created event of the feeds page was called";
     var source = "Template.feeds_page";
     var audience = "";
-    var activity = new Greenlight.Activity(title, description, source, audience);
+    var activity = new Greenlight.Activity({title:title, description:description, source:source, audience:audience});
 
     activity.save();
 
